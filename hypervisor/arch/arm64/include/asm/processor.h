9fec3b80879500ab0f2d694aaacf60e8a42f6a59
Andrea Bastoni <andrea.bastoni@tum.de>
2020-12-08 11:47:01 +0100

    arm64: add helper for interrupt enable/disable/state check, wfe
    Signed-off-by: Andrea Bastoni <andrea.bastoni@tum.de>
    Signed-off-by: Andrea Bastoni <abastoni@minervasys.tech>

--- hypervisor/arch/arm64/include/asm/processor.h
+++ hypervisor/arch/arm64/include/asm/processor.h
@@ -24,6 +24,11 @@
 	0xd503207f, /* 1: wfi  */	\
 	0x17ffffff, /*    b 1b */
 
+#define ARM_DAIF_F	(1 << 6)
+#define ARM_DAIF_I	(1 << 7)
+#define ARM_DAIF_A	(1 << 8)
+#define ARM_DAIF_D	(1 << 9)
+
 #ifndef __ASSEMBLY__
 
 union registers {
@@ -37,6 +42,37 @@ union registers {
 		unsigned long usr[NUM_USR_REGS];
 	};
 };
+
+/* ARMv8 wfe, v7 may require a dsb */
+#define arm_wfe()	asm volatile("wfe" : : : "memory")
+
+static inline unsigned long arm_irq_get_state(void)
+{
+	unsigned long flags;
+
+	asm volatile("mrs %0, daif" : "=&r"(flags) : : "memory");
+
+	return flags;
+}
+
+static inline bool arm_is_irq_off(void)
+{
+	return arm_irq_get_state() & ARM_DAIF_I;
+}
+
+static inline unsigned long arm_irq_save(void)
+{
+	unsigned long flags = arm_irq_get_state();
+
+	asm volatile("msr daifset, #0x2" : : : "memory");
+
+	return flags;
+}
+
+static inline void arm_irq_restore(unsigned long flags)
+{
+	asm volatile("msr daif, %0" : : "r"(flags) : "memory");
+}
 
 #endif /* !__ASSEMBLY__ */
 
