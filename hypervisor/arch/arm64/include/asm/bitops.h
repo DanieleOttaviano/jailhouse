2658561463ce23d306e73e414cffe756d193cefa
Andrea Bastoni <andrea.bastoni@tum.de>
2020-12-09 10:35:49 +0100

    arm64: provide atomic_cas()
    Signed-off-by: Andrea Bastoni <andrea.bastoni@tum.de>
    Signed-off-by: Andrea Bastoni <abastoni@minervasys.tech>

--- hypervisor/arch/arm64/include/asm/bitops.h
+++ hypervisor/arch/arm64/include/asm/bitops.h
@@ -15,6 +15,7 @@
 
 /* also include from arm-common */
 #include_next <asm/bitops.h>
+#include <asm/processor.h>
 
 static inline int atomic_test_and_set_bit(int nr, volatile unsigned long *addr)
 {
@@ -44,6 +45,35 @@ static inline int atomic_test_and_set_bit(int nr, volatile unsigned long *addr)
 			: "r" (1ul << nr));
 	} while (ret);
 	return !!(test);
+}
+
+/*
+ * Usage: if (atomic_cas(addr, old, new) != old): failure.
+ * tmp = *addr;
+ * if (tmp == old) *addr = new;
+ * return tmp;
+ *
+ * acquire + release semantic, no spurious returns (see
+ * https://gcc.gnu.org/onlinedocs/gcc/_005f_005fatomic-Builtins.html#g_t_005f_005fatomic-Builtins)
+ */
+static inline u32 atomic_cas(u32 *addr, u32 old, u32 new)
+{
+	u32 tmp, oldval;
+
+	dmb(ish);
+	asm volatile (
+		"1: ldxr	%w1, [%2]\n\t"
+		"subs		%w0, %w1, %w3\n\t"
+		"b.ne		2f\n\t"
+		"stxr		%w0, %w4, [%2]\n\t" /* 1 on failure */
+		"cbnz		%w0, 1b\n\t"
+		"2:\n\t"
+		: "=&r"(tmp), "=&r"(oldval)
+		: "r"(addr), "r"(old), "r"(new)
+		: "memory", "cc");
+	dmb(ish);
+
+	return oldval;
 }
 
 #endif
