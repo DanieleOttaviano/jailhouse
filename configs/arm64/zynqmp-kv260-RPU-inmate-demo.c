b6dacdd3f6842dad5b9a6e0af6177bffe4a7d645
Daniele Ottaviano <danieleottaviano97@gmail.com>
2023-09-20 17:28:55 -0400

    added stream_ids to RPU demo
    Signed-off-by: Daniele Ottaviano <danieleottaviano97@gmail.com>

--- configs/arm64/zynqmp-kv260-RPU-inmate-demo.c
+++ configs/arm64/zynqmp-kv260-RPU-inmate-demo.c
@@ -21,6 +21,7 @@ struct {
 	__u64 cpus[1];
 	__u64 rpus[1];
 	struct jailhouse_memory mem_regions[5];
+	union jailhouse_stream_id stream_ids[2];
 } __attribute__((packed)) config = {
 	.cell = {
 		.signature = JAILHOUSE_CELL_DESC_SIGNATURE,
@@ -33,6 +34,7 @@ struct {
 		.num_memory_regions = ARRAY_SIZE(config.mem_regions),
 		.num_irqchips = 0,
 		.num_pci_devices = 0,
+		.num_stream_ids = ARRAY_SIZE(config.stream_ids),
 
 		.console = {
 			.address = 0xff010000,
@@ -87,5 +89,17 @@ struct {
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
 				JAILHOUSE_MEM_COMM_REGION,
 		},
+	},
+
+  .stream_ids = {
+    {
+			.mmu500.id = 0x800,
+			.mmu500.mask_out = 0xf,
+		},
+		{
+			.mmu500.id = 0x810,
+			.mmu500.mask_out = 0xf,
+		},
 	}
+
 };
