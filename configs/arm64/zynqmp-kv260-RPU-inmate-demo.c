b304505ed9f31f30947c687add58df3da3ffecd0
Daniele Ottaviano <danieleottaviano97@gmail.com>
2023-10-30 15:04:12 -0400

    adjustment to RPU0 memory (linker script + config cell)
    Signed-off-by: Daniele Ottaviano <danieleottaviano97@gmail.com>

--- configs/arm64/zynqmp-kv260-RPU-inmate-demo.c
+++ configs/arm64/zynqmp-kv260-RPU-inmate-demo.c
@@ -79,7 +79,7 @@ struct {
 		/* RAM */ {
 			.phys_start = 0x3ed00000,
 			.virt_start = 0,
-			.size = 0x6400000,
+			.size = 0x4000000,
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
 				JAILHOUSE_MEM_EXECUTE | JAILHOUSE_MEM_LOADABLE,
 		},
@@ -91,6 +91,7 @@ struct {
 		},
 	},
 
+	//TO REMOVE
   .stream_ids = {
     {
 			.mmu500.id = 0x800,
