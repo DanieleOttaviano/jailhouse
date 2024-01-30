7d176ca3a5436c660909318609a0c170351591c3
Daniele Ottaviano <danieleottaviano97@gmail.com>
2024-01-30 18:48:19 -0500

    RISC-V boot demo for Omnivisor integrated
    Signed-off-by: Daniele Ottaviano <danieleottaviano97@gmail.com>

---
+++ inmates/demos/riscv/src_boot/riscv-demo.c
@@ -0,0 +1,22 @@
+
+#include <stdint.h>
+
+#define DIM_MB 90
+#define SIZE_NO_BUFFER 82       // Dimension of the bin without the buffer the buffer
+#define SIZE_BIN 1024*DIM_MB    // Dimension of the bin file
+
+static volatile const char buffer[(SIZE_BIN-SIZE_NO_BUFFER)*1024]={0};
+
+void main(void){
+	volatile uint32_t* system_counter = (uint32_t*)0xFF250000;
+	volatile uint32_t *shared_memory = (uint32_t *)0x46d00000;
+
+	// Boot Time
+	uint32_t time = *system_counter;
+
+	// Write boot time in shared memory
+  	shared_memory[0] = time;
+	
+	while(1);
+}
+
