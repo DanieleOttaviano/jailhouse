71944ffb02c39a4571407c4ddb38c0138e5f9b68
Daniele Ottaviano <danieleottaviano97@gmail.com>
2024-01-31 11:13:44 -0500

    Added RISC-V membomb demo
    Signed-off-by: Daniele Ottaviano <danieleottaviano97@gmail.com>

--- inmates/demos/riscv/src_baremetal/riscv-demo.c
+++ inmates/demos/riscv/src_baremetal/riscv-demo.c
@@ -1,25 +1,53 @@
 
 #include <stdint.h>
 
-volatile static uint32_t * const SHM = (uint32_t *)0x78FF0000;
+#define NPAGES 1024
+#define DIM 12 * NPAGES // 123*32(size fo int)=4096 bytes= 4KB(size of one page)
+#define REP 1 // 10 
+#define REP_TIME 100
+#define FREQUENCY 100  // 100 MHz
+#define PERIOD 200000 // 200 ms
 
 void main(void){
 	volatile uint32_t* system_counter = (uint32_t*)0xFF250000;
 	volatile uint32_t *shared_memory = (uint32_t *)0x46d00000;
+	uint32_t *mem_array = (uint32_t *)0x78FF0000;
+	uint32_t start, end, diff;
+	uint32_t readsum = 0;
+  	uint32_t time_us = 0;
+  	int i, j, k;
 
-	// Boot Time
-	uint32_t time = *system_counter;
-
-	// Write boot time in shared memory
-  	shared_memory[0] = time;
-
-	//DEBUG SHM
-	SHM[0] = 0xDEADBEEF;
-	SHM[1] = time;
-	
-	while(1){
-		time = *system_counter;
-		shared_memory[0] = time;
+	// Mem Array Initialization
+	for (i = 0; i < DIM; i++) {
+		mem_array[i] = i;
 	}
-}
+	//SHM Array initialization
+	for (i = 0; i < REP_TIME; i++) {
+		shared_memory[i] = 0;
+	}
 
+	for(k = 0; k < REP_TIME; k++ ){ 
+		/* Actual access */
+		start = *system_counter;
+		for (j = 0; j < REP; j++) {
+			for (i = 0; i < DIM; i++) {
+				readsum += mem_array[i]; // READ
+			}
+		}
+		end = *system_counter;
+		// Calculate time in us
+		diff = end - start;
+		time_us = diff / FREQUENCY;
+
+		shared_memory[k] = time_us;
+
+		// wait until the end of the period
+		while (time_us < PERIOD){
+			end = *system_counter;
+			diff = end - start;
+			time_us = diff / FREQUENCY;
+		}
+	}
+	
+	while(1);
+}
---
+++ inmates/demos/riscv/src_membomb/riscv-demo.c
@@ -0,0 +1,36 @@
+#include <stdint.h>
+
+#define REP_TIME 100000 // 10
+#define FREQUENCY 100  // 100 MHz
+#define PERIOD 30 // 30 us
+
+static uint32_t memory_size = 4 * 1024 * 1024; 			// 4Mb
+static uint8_t* base_address = (uint8_t *)0x78FF0000;
+
+void main(void){
+	volatile uint32_t* system_counter = (uint32_t*)0xFF250000;
+	volatile uint32_t* shared_memory =  (uint32_t *)0x46d00000;
+	uint32_t start, end, diff;
+  	uint32_t time_us = 0;
+  	int i, j, k;
+	
+	while(1){
+		for(int i = 0 ; i < memory_size ; i=i+16){
+
+			start=*system_counter;
+			base_address[i] = 0xFF;
+			end=*system_counter;
+
+			// Calculate time in us
+			diff = end - start;
+			time_us = diff / FREQUENCY;
+
+			// wait until the end of the period
+			while (time_us < PERIOD){
+				end=*system_counter;
+				diff = end - start;
+				time_us = diff / FREQUENCY;
+			}
+		}
+	}
+}
