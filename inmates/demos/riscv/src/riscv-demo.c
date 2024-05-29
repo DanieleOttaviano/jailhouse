f85e3aa4a4a23c3c459f815fa0a3dc09637e2505
Daniele Ottaviano <danieleottaviano97@gmail.com>
2024-05-29 06:55:10 -0400

    adjusted RPU and RISCV Tests
    Signed-off-by: Daniele Ottaviano <danieleottaviano97@gmail.com>

--- inmates/demos/riscv/src/riscv-demo.c
+++ inmates/demos/riscv/src/riscv-demo.c
@@ -1,7 +1,7 @@
 #include <stdint.h>
 
+#define REP_TIME 1
 #define FREQUENCY 100  // 100 MHz
-#define I_FREQUENCY 1/FREQUENCY  // 100 MHz
 
 #define CALL(NAME, FUN) CONCAT(NAME, FUN)
 #define CONCAT(NAME, FUN)  NAME ## _ ## FUN()
@@ -13,31 +13,33 @@
 
 #include INCLUDE_FILE(BENCHNAME)
 
-void main(void) {
+void main(void){
+	volatile uint32_t* system_counter = (uint32_t*)0xFF250000;
+	volatile uint32_t* shared_memory = (uint32_t *)0x46d00000;
+ 	uint32_t start, end, diff;
+	uint32_t time_us = 0;
+	int i;
 
-	volatile uint64_t* system_counter = (uint64_t*)0xFF250000;
-	volatile uint64_t* shared_memory = (uint64_t *)0x46d00000;
- 	uint64_t start, end, diff;
+	//SHM Array initialization
+	for (i = 0; i < REP_TIME; i++) {
+		shared_memory[i] = 0;
+	}
 
-	shared_memory[0] = 0;
-	shared_memory[1] = 0;
+	for(i = 0; i < REP_TIME; i++ ){ 
+		/* Actual access */
+		start = *system_counter;
+		// Exec the Benchmark 
+		CALL(BENCHNAME, init);
+		CALL(BENCHNAME, main);
 
-	/* Actual access */
-	start = *system_counter;
+		end = *system_counter;
 
-	// Exec the Benchmark 
-	CALL(BENCHNAME, init);
-	CALL(BENCHNAME, main);
+		// Calculate time in us    
+		diff = end - start;
+		time_us = diff / FREQUENCY;
 
-	end = *system_counter;
-
-	// Calculate time in us    
-	diff = end - start;
-
-	// time_us = diff * I_FREQUENCY;
-
-	shared_memory[0] = diff;
-	shared_memory[1] = FREQUENCY;
+		shared_memory[i] = time_us;
+	}
 
 	while(1);
 }
