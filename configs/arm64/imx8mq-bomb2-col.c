829075c7c8784bee7ab97fdee1ac42c321994a4b
Filippo Fontana <filippo.fontana@minervasys.tech>
2024-06-12 18:59:37 +0200

    00100#584: [configs, arm64, imx8mq] add cache coloring support
    This commit adds the configurations for the cells which implements
    coloring on the imx8mq. This includes also the colored memory-bomb
    cells required for testing.
    
    Signed-off-by: Filippo Fontana <filippo.fontana@minervasys.tech>
    Signed-off-by: Andrea Bastoni <abastoni@minervasys.tech>

---
+++ configs/arm64/imx8mq-bomb2-col.c
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#include <jailhouse/types.h>
+#include <jailhouse/cell-config.h>
+
+#define BOMB_ID	2
+#define BOMB_COLORS 0x00f
+#include <jailhouse/mem-bomb.h>
+#include <imx8mq-bomb-col-template.h>
