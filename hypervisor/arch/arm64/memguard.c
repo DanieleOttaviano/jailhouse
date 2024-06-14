80c6c870c396fbd668600831a64369283cd300d1
Filippo Fontana <filippo.fontana@minervasys.tech>
2024-06-14 17:44:12 +0200

    00100#561: [hypervisor, arm64, memguard] update header's copyrights and authors
    Signed-off-by: Filippo Fontana <filippo.fontana@minervasys.tech>
    Signed-off-by: Andrea Bastoni <abastoni@minervasys.tech>

--- hypervisor/arch/arm64/memguard.c
+++ hypervisor/arch/arm64/memguard.c
@@ -1,9 +1,10 @@
 /*
- * Memguard for Jailhouse ARM64, GICv2
+ * Memguard for Jailhouse ARM64, GICv2, GICv3
  *
  * Copyright (c) Czech Technical University in Prague, 2018
  * Copyright (C) Boston University, 2020
  * Copyright (C) Technical University of Munich, 2020
+ * Copyright (C) Minerva Systems, 2024
  *
  * Authors:
  *  Joel Matějka <matejjoe@fel.cvut.cz>
@@ -11,6 +12,7 @@
  *  Přemysl Houdek <houdepre@fel.cvut.cz>
  *  Renato Mancuso (BU) <rmancuso@bu.edu>
  *  Andrea Bastoni <andrea.bastoni@tum.de> at https://rtsl.cps.mw.tum.de
+ *  Filippo Fontana <filippo.fontana@minervasys.tech>
  *
  * This work is licensed under the terms of the GNU GPL, version 2.  See
  * the COPYING file in the top-level directory.
