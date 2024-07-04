2034d81fb3f94a4bdf785b1371d822345b65000a
Filippo Fontana <filippo.fontana@minervasys.tech>
2024-07-04 14:46:24 +0200

    00100#11: [configs, arm64] add support to zynmp-zcu106 cells
    This commit adds the standard file-suite (i.e. root cell, colored root
    cell, inmate demo and memory bombs) for the zynmp-zcu106. Basically the
    files are the same of the generic ZYNQMP with the updated memory regions
    (the ZCU106 has 4 GB of DDR).
    
    Signed-off-by: Filippo Fontana <filippo.fontana@minervasys.tech>
    Signed-off-by: Mirko Cangiano <mirko.cangiano@minervasys.tech>
    Signed-off-by: Luca Palazzi <luca.palazzi@minervasys.tech>
    Signed-off-by: Carlo Nonato <carlo.nonato@minervasys.tech>
    Signed-off-by: Donato Ferraro <donato.ferraro@minervays.tech>
    Signed-off-by: Fabio Spanò <fabio.spano@minervasys.tech>
    Signed-off-by: Andrea Bastoni <abastoni@minervasys.tech>

---
+++ configs/arm64/zynqmp-zcu106-inmate-demo.c
@@ -0,0 +1,74 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Configuration for demo inmate on Xilinx ZynqMP ZCU106 eval board:
+ * 1 CPU, 64K RAM, 1 serial port
+ *
+ * Copyright (c) Minerva Systems, 2022
+ *
+ * Authors:
+ *   Mirko Cangiano <mirko.cangiano@minervasys.tech>
+ *   Luca Palazzi <luca.palazzi@minervasys.tech>
+ *   Carlo Nonato <carlo.nonato@minervasys.tech>
+ *   Donato Ferraro <donato.ferraro@minervays.tech>
+ *   Fabio Spanò <fabio.spano@minervasys.tech>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#include <jailhouse/types.h>
+#include <jailhouse/cell-config.h>
+
+struct {
+	struct jailhouse_cell_desc cell;
+	__u64 cpus[1];
+	struct jailhouse_memory mem_regions[3];
+} __attribute__((packed)) config = {
+	.cell = {
+		.signature = JAILHOUSE_CELL_DESC_SIGNATURE,
+		.revision = JAILHOUSE_CONFIG_REVISION,
+		.architecture = JAILHOUSE_ARM64,
+		.name = "inmate-demo",
+		.flags = JAILHOUSE_CELL_PASSIVE_COMMREG |
+			JAILHOUSE_CELL_VIRTUAL_CONSOLE_ACTIVE,
+
+		.cpu_set_size = sizeof(config.cpus),
+		.num_memory_regions = ARRAY_SIZE(config.mem_regions),
+		.num_irqchips = 0,
+
+		.console = {
+			.address = 0xff010000,
+			.type = JAILHOUSE_CON_TYPE_XUARTPS,
+			.flags = JAILHOUSE_CON_ACCESS_MMIO |
+				 JAILHOUSE_CON_REGDIST_4,
+		},
+	},
+
+	.cpus = {
+		0x8,
+	},
+
+	.mem_regions = {
+		/* UART */ {
+			.phys_start = 0xff010000,
+			.virt_start = 0xff010000,
+			.size = 0x1000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_IO | JAILHOUSE_MEM_ROOTSHARED,
+		},
+		/* RAM */ {
+			.phys_start = 0x50600000,
+			.virt_start = 0,
+			.size = 0x00010000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_EXECUTE | JAILHOUSE_MEM_LOADABLE,
+		},
+		/* communication region */ {
+			.virt_start = 0x80000000,
+			.size = 0x00001000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_COMM_REGION,
+		},
+	},
+};
