43074b19bafe8cb68d3370c07ff8645285aae8ea
Anna Amodio <amod.anna0@gmail.com>
2024-10-27 11:51:32 +0100

    add fpga in jailhouse-cell-linux

--- pyjailhouse/cell.py
+++ pyjailhouse/cell.py
@@ -14,6 +14,21 @@ import errno
 import fcntl
 import struct
 
+def config_omnv_fpga():
+    filename = os.path.dirname(os.path.abspath(__file__)) + "/../include/config.h"
+    with open(filename, 'r') as f:
+        for line in f:
+            if line.startswith(f"#define CONFIG_OMNV_FPGA"):
+                return True
+        return False
+
+
+class JailhousePreloadBitstream(ctypes.Structure):
+    _fields_ = [
+        ('region', ctypes.c_uint32),
+        ('name', ctypes.c_char * 32), 
+    ]
+
 
 class JailhouseCell:
     JAILHOUSE_CELL_CREATE = 0x40100002
@@ -35,14 +50,28 @@ class JailhouseCell:
             if e.errno != errno.EEXIST:
                 raise e
 
-    def load(self, image, address):
-        cbuf = ctypes.create_string_buffer(bytes(image))
+    def load_image(self, image, address):
 
-        load = struct.pack('i4x32sI4xQQQ8x',
+        cbuf = ctypes.create_string_buffer(bytes(image))
+        if config_omnv_fpga():
+            load = struct.pack('i4x32sIIPQQQ8x',
+                           JailhouseCell.JAILHOUSE_CELL_ID_UNUSED, self.name,
+                           1,0,0,ctypes.addressof(cbuf), len(image), address)
+        else:
+            load = struct.pack('i4x32sI4xQQQ8x',
                            JailhouseCell.JAILHOUSE_CELL_ID_UNUSED, self.name,
                            1, ctypes.addressof(cbuf), len(image), address)
         fcntl.ioctl(self.dev, self.JAILHOUSE_CELL_LOAD, load)
 
+    
+    def load_bitstream(self, bitstream, region):
+        bitstream = JailhousePreloadBitstream(region=region, name=bitstream)
+
+        load = struct.pack('i4x32sIIPQQQ8x',
+                           JailhouseCell.JAILHOUSE_CELL_ID_UNUSED, self.name,
+                           0,1,ctypes.pointer(bitstream),0,0,0)
+        fcntl.ioctl(self.dev, self.JAILHOUSE_CELL_LOAD, load)
+    
     def start(self):
         start = struct.pack('i4x32s', JailhouseCell.JAILHOUSE_CELL_ID_UNUSED,
                             self.name)
