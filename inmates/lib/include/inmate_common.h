4ebf94f41bdddbeb332a2ad0fe2b208540d55dbc
Daniele Ottaviano <daniele.ottaviano@tum.de>
2025-02-26 10:11:02 +0100

    Disable definition of bool type if already defined
    Signed-off-by: Daniele Ottaviano <daniele.ottaviano@tum.de>

--- inmates/lib/include/inmate_common.h
+++ inmates/lib/include/inmate_common.h
@@ -84,11 +84,12 @@ typedef u32 __u32;
 typedef s64 __s64;
 typedef u64 __u64;
 
-//MARTe2 compatibility
 #ifdef __cplusplus
 	#include <stdbool.h>
-#else 
+#else
+#if !defined(true) && !defined(false) && !defined(bool) 
 	typedef enum { true = 1, false = 0 } bool;
+#endif
 #endif
 
 #include <jailhouse/hypercall.h>
